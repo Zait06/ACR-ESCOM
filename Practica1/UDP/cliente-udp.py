1ab084cb7d324514984970de2b548a604280f607
Zait06 <zait-hdez@hotmail.com>
2019-08-18 12:07:31 -0500

    Cliente-Servidor-UDP

--- Practica1/UDP/cliente-udp.py
+++ Practica1/UDP/cliente-udp.py
@@ -1,5 +1,8 @@
 import socket
-from JuegoGato.gato import *
+import sys
+import os
+sys.path.append(os.path.abspath('../JuegoGato'))    # Subir a la capeta correspondiente para poder importar el gato
+from gato import *
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 54321  # The port used by the server
