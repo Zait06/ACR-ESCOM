7b5b81c0bd63fb3f6636c88c9e285808448f59e8
Zait06 <zait-hdez@hotmail.com>
2019-10-17 18:57:35 -0500

    Servidor HTTP

--- Practica4/Servidor/servidorHTTP.js
+++ Practica4/Servidor/servidorHTTP.js
@@ -3,15 +3,15 @@ var http=require("http"),  // Protocolo
 
 var peticion=function(solicitud,respuesta){	// Función de la respuesta
 	console.log("Solicitud pedida con el metodo %s",solicitud['method']);	// Solicitud al servidor
-	//console.log(solicitud)
+	console.log(solicitud['headers'])
 	fs.readFile("./pagina.html",function(err,html){
 		var html_str=html.toString();
-		var variables=html_str.match(/[^\{\}]+(?=\})/g);
-		// respuesta.writeHead(200,{'Content-Type':'text/html'})
-		respuesta.write(html_str);	// Lectura de HTML
+		respuesta.write("Tarea hecha");	// Lectura de HTML
 		respuesta.end();
 	})
 };
+
+console.log("Servidor a la escucha")
 
 var servidor=http.createServer(peticion); // Crea servidor
 
