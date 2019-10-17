2c6c98d4ef75dac7a0426060266168b6c12f8416
Zait06 <zait-hdez@hotmail.com>
2019-10-17 18:57:57 -0500

    Servidor HTTP

--- Practica4/Cliente/clienteHTTP.py
+++ Practica4/Cliente/clienteHTTP.py
@@ -4,7 +4,7 @@ import argparse
 import http.client
 
 def aplicarGET(conn):
-     print("--- Metodo GET ---")
+     print("\n\n--- Metodo GET ---")
      conn.request("GET","/")
      r = conn.getresponse()
      print(r.status, r.reason)
@@ -15,19 +15,26 @@ def aplicarHEAD(conn):
      conn.request("HEAD","/")
      r=conn.getresponse()
      print(r.status, r.reason)
-     print("HEAD hecho? R=",str(r.read().decode()==''))
+     print("HEAD hecho? R =",str(r.read().decode()==''))
 
-def aplicarPOST(coon):
+def aplicarPOST(conn):
      print("\n\n--- Metodo POST ---")
      nombre=input("Ingrese un nombre: ")
      apelli=input("Ingrese su apellido: ")
-     headers = {'Content-type': 'application/json'}
+     headers = {'Content-Type':'text/html'}
      foo = {'nombre':nombre,'apellido':apelli}
      json_data = json.dumps(foo)
-     conn.request('POST', '/post', json_data, headers)
-     r1 = conn.getresponse()
-     print(r1.status, r1.reason)
-     print(r1.read().decode())
+     conn.request('POST', '/', json_data, headers)
+     r = conn.getresponse()
+     print(r.status, r.reason)
+     print(r.read().decode())
+
+def aplicarPUT(conn):
+     print("\n\n--- Metodo PUT ---")
+     conn.request("PUT","/","Metodo PUT")
+     r=conn.getresponse()
+     print(r.status, r.reason)
+     print(r.read().decode())
 
 os.system("clear")
 
@@ -35,20 +42,26 @@ REMOTE_SERVER_HOST=input("Ingrese la 'ip:puerto' del servidor: ")
 REMOTE_SERVER_PATH='/'
 
 conn = http.client.HTTPConnection(REMOTE_SERVER_HOST)
+os.system("clear")
 
-while True:
-     metodo=input("\nIngrese el metodo deseado: ")
-     os.system("clear")
-     try:
-          if metodo.upper()=='GET':
-              aplicarGET(conn)
-          elif metodo.upper()=='HEAD':
-               aplicarHEAD(conn)
-          elif metodo.upper()=='POST':
-               aplicarPOST(conn)
-          elif metodo.upper()=='EXIT':
-               break
-     except Exception as e:
-          print(e)
-     finally:
-          conn.close()
+if conn:
+     while True:
+          metodo=input("\nIngrese el metodo deseado: ")
+          os.system("clear")
+          try:
+               if metodo.upper()=='GET':
+                    aplicarGET(conn)
+               elif metodo.upper()=='HEAD':
+                    aplicarHEAD(conn)
+               elif metodo.upper()=='POST':
+                    aplicarPOST(conn)
+               elif metodo.upper()=='PUT':
+                    aplicarPUT(conn)
+               elif metodo.upper()=='EXIT':
+                    break
+               else:
+                    print("Instruccion no encontrada, por favor, intente de nuevo.")
+          except Exception as e:
+               print(e)
+          finally:
+               conn.close()
