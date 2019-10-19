c396e17baade35382d7b23f448b7cc247bc40d13
Zait06 <zait-hdez@hotmail.com>
2019-10-19 12:23:34 -0500

    Cliente-Servidor TFTP

--- Practica4/Servidor/servidorTFTP.py
+++ Practica4/Servidor/servidorTFTP.py
@@ -1,6 +1,8 @@
 import tftpy
+import logging
 # se crea la instancia del servidor tftp
 # el unico parametro es la ruta donde estara la carpeta o directorio donde se almacenaran nuestros archivos
 server = tftpy.TftpServer('./recursosTFTP')
 print('Servidor iniciado')
+logging.basicConfig(level=logging.INFO, format='(ServidorTFTP) %(message)s',)
 server.listen('127.0.0.1', 6969)
