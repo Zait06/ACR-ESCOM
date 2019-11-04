a38b9348573d509090521c46b87574e413bee010
Zait06 <zait-hdez@hotmail.com>
2019-11-04 08:11:00 -0600

    Servidor RPC

--- Practica5/Servidor/instrucciones.py
+++ Practica5/Servidor/instrucciones.py
@@ -1,14 +1,23 @@
 import os
 import sys
 
-class Instrucciones:
-    def __init__(self):
-        self.mensaje=list()
-        self.orden=['crear','mover','copiar']
-        self.winOrd=['mkdir','put']
+# http://archive.download.redhat.com/pub/redhat/linux/7.1/es/doc/RH-DOCS/es/rhl-gsg-es-7.1/ch-doslinux.html
 
+os.chdir('user')
+
+class Instrucciones:
     def eleccion(self, inst):
         self.ins=inst.split()
 
-    def show_type(self, arg):
-        return (str(arg), str(type(arg)), arg)
+    def verDireccion(self):
+        return str(os.getcwd())
+    
+    def verContenido(self):
+        return str(os.listdir())
+
+    def crearCarpeta(self,nombre):
+        os.mkdir(str(nombre))
+        return self.verContenido()
+
+    def copiarArch(self):
+        return 'copiado'
