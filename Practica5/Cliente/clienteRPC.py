58c696b5bab07dbec97b79ce331bb0e96b542817
Zait06 <zait-hdez@hotmail.com>
2019-11-05 09:42:44 -0600

    Cliente RPC

--- Practica5/Cliente/clienteRPC.py
+++ Practica5/Cliente/clienteRPC.py
@@ -1,40 +1,35 @@
 import xmlrpc.client
 import datetime
+import getpass
 
-s = xmlrpc.client.ServerProxy('http://localhost:8000')  # Quien atiende las solicitudes
+s = xmlrpc.client.ServerProxy('http://127.0.0.1:8080')  # Quien atiende las solicitudes
 orden=""; instruc=list()
 
-while True:
-    orden=input("user>> ")
-    instruc=orden.lower().split()
+user=input("Usuario: ")
+pasw=getpass.getpass("Contraseña: ")
+print(pasw)
+ing=s.signIn(user,pasw)
 
-    if instruc[0]=='ls' or instruc[0]=='dir':
-        print(s.verContenido())
-    elif instruc[0]=='mkdir' or instruc[0]=='md':
-        print(s.crearCarpeta(instruc[1]))
-    elif instruc[0]=='pwd':
-        print(s.verDireccion())
-    elif instruc[0]=='exit':
-        break
+try:
+    while ing:
+        orden=input("user@"+user+">>")
+        instruc=orden.lower().split()
 
-'''
-data = [
-    ('boolean', True),
-    ('integer', 1),
-    ('float', 2.5),
-    ('string', 'some text'),
-    ('datetime', datetime.datetime.now()),
-    ('array', ['a', 'list']),
-    ('array', ('a', 'tuple')),
-    ('structure', {'a': 'dictionary'}),
-]
-
-for t, v in data:
-    as_string, type_name, value = s.show_type(v)
-    print('{:<12}: {}'.format(t, as_string))
-    print('{:12}  {}'.format('', type_name))
-    print('{:12}  {}'.format('', value))
-
-# Print list of available methods
-print(s.system.listMethods())
-'''
+        if instruc[0]=='readdir':
+            print(s.verContenido())
+        elif instruc[0]=='mkdir':
+            print(s.crearCarpeta(instruc[1]))
+        elif instruc[0]=='create':
+            print(s.crearArchivo(instruc[1]))
+        elif instruc[0]=='pwd':
+            print(s.verDireccion())
+        elif instruc[0]=='exit':
+            break
+        else:
+            print("Instruccion incorrecta, intente de nuevo\n")
+except Exception as e:
+    print(e)
+else:
+    print("Usuario o contraseña incorrectas")
+finally:
+    print("Adios")
