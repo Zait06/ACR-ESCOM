3dba8fbe2d53c826d482d9426877abde23185c31
Zait06 <zait-hdez@hotmail.com>
2019-11-05 09:42:25 -0600

    Servidor RPC

--- Practica5/Servidor/instrucciones.py
+++ Practica5/Servidor/instrucciones.py
@@ -6,6 +6,17 @@ import sys
 os.chdir('user')
 
 class Instrucciones:
+    def signIn(self,usua,pasw):
+        perfil=list(); simon=False
+        f=open("../perfiles.txt","r")
+        for linea in f.readlines():
+            perfil=str(linea).split(':')
+            if perfil[0]==usua and perfil[1]==(str(pasw)+"\n"):
+                simon=True
+                break
+        f.close()
+        return simon
+
     def eleccion(self, inst):
         self.ins=inst.split()
 
@@ -14,6 +25,11 @@ class Instrucciones:
     
     def verContenido(self):
         return str(os.listdir())
+    
+    def crearArchivo(self,arch):
+        f=open(arch,"w")
+        f.close()
+        return self.verContenido()
 
     def crearCarpeta(self,nombre):
         os.mkdir(str(nombre))
--- Practica5/Servidor/servidorRPC.py
+++ Practica5/Servidor/servidorRPC.py
@@ -6,7 +6,7 @@ class RequestHandler(SimpleXMLRPCRequestHandler):   # Escucha a todos los solici
     rpc_paths = ('/RPC2',)  # Ruta principal
 
 # Crea el servidor RPC
-with SimpleXMLRPCServer(('localhost', 8000),requestHandler=RequestHandler) as server:
+with SimpleXMLRPCServer(('127.0.0.1', 8080),requestHandler=RequestHandler) as server:
     server.register_introspection_functions()
 
     server.register_instance(Instrucciones()) # La clase se registra con todos sus metodos de forma xml
