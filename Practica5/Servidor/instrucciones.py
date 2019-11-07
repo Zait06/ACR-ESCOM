eb4ecb7d927485bb45aab54d307dfa706a9fc70b
Zait06 <zait-hdez@hotmail.com>
2019-11-06 18:46:36 -0600

    Servidor RPC

--- Practica5/Servidor/instrucciones.py
+++ Practica5/Servidor/instrucciones.py
@@ -1,14 +1,9 @@
 import os
-import sys
-
-# http://archive.download.redhat.com/pub/redhat/linux/7.1/es/doc/RH-DOCS/es/rhl-gsg-es-7.1/ch-doslinux.html
+import time
 
 os.chdir('user')
 
 class Instrucciones:
-    def __init__(self):
-        self.direc=''
-
     def signIn(self,usua,pasw):
         perfil=list(); simon=False
         f=open("../perfiles.txt","r")
@@ -18,31 +13,69 @@ class Instrucciones:
                 simon=True
                 break
         f.close()
-        self.direc=usua
         return simon
 
-    def hacerPing(self):
-        return 'Conectado'
+    def logIn(self,usua,pasw):
+        try:
+            with open("../perfiles.txt","a") as f:
+                f.write(usua+':'+pasw+'\n')
+            os.mkdir(usua)
+            with open("./"+usua+"/inicio.txt",'w') as f:
+                f.write("Bienvenid@ "+usua+" al servicio RPC")
+        except Exception as e:
+            print(e)
+        return True
 
-    def verDireccion(self):
-        return str(os.getcwd())
-    
-    def verContenido(self):
-        return str(os.listdir())
-    
-    def crearArchivo(self,arch):
-        f=open(arch,"w")
+    def hacerPing(self):                            # null - ping
+        return 'Conectado al servidor'
+
+    def crearArchivo(self,arch,usua):               # create
+        f=open("./"+usua+"/"+arch,"w")
         f.close()
-        return self.verContenido()
+        return self.verContenido(usua)
+    
+    def ordenLookUp(self,usua,ficha):               # lookup
+        return 'Informacion'
 
-    def renomCarpeta(self,nom1,nom2):
-        os.rename(nom1,nom2)
-        return self.verContenido()
+    def leerArchivo(self,usua,arch):                # read
+        envio=str.encode('')
+        with open("./"+usua+"/"+arch,"rb") as f:
+            for linea in f.readlines():
+                print(linea)
+                envio=envio+linea
+        return envio
 
-    def crearCarpeta(self,nombre):
-        os.mkdir(str(nombre))
-        return self.verContenido()
+    def editarArchivo(self,usua,arch,texto):        # write
+        with open("./"+usua+"/"+arch,"a") as f:
+            f.write(texto+'\n')
+        return self.leerArchivo(usua,arch)
 
-    def borrarCarpeta(self,nombre):
-        os.rmdir(nombre)
-        return self.verContenido()
+    def renomCarpeta(self,nom1,nom2,usua):          # rename
+        os.rename("./"+usua+"/"+nom1,"./"+usua+"/"+nom2)
+        return self.verContenido(usua)
+
+    def borrarArchivo(self,nom,usua):               # remove
+        os.remove("./"+usua+"/"+nom)
+        return self.verContenido(usua)
+
+    def crearCarpeta(self,nombre,usua):             # mkdir
+        os.mkdir("./"+usua+"/"+nombre)
+        return self.verContenido(usua)
+
+    def borrarCarpeta(self,nombre,usua):            # rmdir
+        os.rmdir("./"+usua+"/"+nombre)
+        return self.verContenido(usua)
+
+    def verContenido(self,usua):                    # readdir
+        return str(os.listdir(usua))
+
+    def infoArchivo(self,nombre,usua):              # getattr
+        mensaje=nombre
+        tama=os.path.getsize("./"+usua+"/"+nombre)
+        hora=os.path.getmtime("./"+usua+"/"+nombre)
+        mensaje=mensaje+'\n\tFecha de modificado: '+str(time.ctime(hora))
+        mensaje=mensaje+'\n\tTamaño: '+str(tama)+' bytes'
+        return mensaje
+
+    def verDireccion(self,usua):                    # pwd
+        return os.getcwd()+"/"+usua
--- Practica5/Servidor/servidorRPC.py
+++ Practica5/Servidor/servidorRPC.py
@@ -1,12 +1,15 @@
+import sys
 from instrucciones import *
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
+
+host=sys.argv[1]; port=int(sys.argv[2])
 
 class RequestHandler(SimpleXMLRPCRequestHandler):   # Escucha a todos los solicitantes http
     rpc_paths = ('/RPC2',)  # Ruta principal
 
 # Crea el servidor RPC
-with SimpleXMLRPCServer(('localhost', 8080),requestHandler=RequestHandler) as server:
+with SimpleXMLRPCServer((host,port),requestHandler=RequestHandler) as server:
     print("Servidor a la escucha")
     server.register_introspection_functions()
 
