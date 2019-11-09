9506098949fb19189c0896af5c9ae4c43a6fabe8
Zait06 <zait-hdez@hotmail.com>
2019-11-09 16:35:17 -0600

    Servidor RPC

--- Practica5/Servidor/instrucciones.py
+++ Practica5/Servidor/instrucciones.py
@@ -6,22 +6,22 @@ os.chdir('user')
 class Instrucciones:
     def signIn(self,usua,pasw):                     # Ingresar con un usuario
         perfil=list(); simon=False
-        f=open("../perfiles.txt","r")
+        f=open("../perfiles.txt","r")               # Abrir archivo de perfiles
         for linea in f.readlines():
             perfil=str(linea).split(':')
-            if perfil[0]==usua and perfil[1]==(str(pasw)+"\n"):
+            if perfil[0]==usua and perfil[1]==(str(pasw)+"\n"): # Verificación de su existencia
                 simon=True
                 break
-        f.close()
+        f.close()                                   # Cierre el archivo
         return simon
 
     def logIn(self,usua,pasw):                      # Registrar nuevo usuario
         simon=False
         try:
-            with open("../perfiles.txt","a") as f:
+            with open("../perfiles.txt","a") as f:  # Abrir archivo para agregar un nuevo miembro
                 f.write(usua+':'+pasw+'\n')
-            os.mkdir(usua)
-            with open("./"+usua+"/inicio.txt",'w') as f:
+            os.mkdir(usua)                          # Se crea una carpeta para el nuevo miembro
+            with open("./"+usua+"/inicio.txt",'w') as f:    # Da archivo de bienvenida
                 f.write("Bienvenid@ "+usua+" al servicio RPC")
             simon=True
         except Exception as e:
@@ -33,12 +33,18 @@ class Instrucciones:
         return 'Conectado al servidor'
 
     def crearArchivo(self,arch,usua):               # create
-        f=open("./"+usua+"/"+arch,"w")
+        f=open("./"+usua+"/"+arch,"w")              # Abre y cierra un archivo
         f.close()
         return self.verContenido(usua)
     
     def ordenLookUp(self,usua,ficha):               # lookup
-        return self.infoArchivo(ficha,usua)
+        encon="Elemento inexistente"
+        conten=os.listdir(usua)
+        for dd in conten:
+            if dd==ficha:
+                encon="Elemento encontrado"
+                break
+        return encon
 
     def leerArchivo(self,usua,arch):                # read
         envio=str.encode('')
@@ -52,7 +58,7 @@ class Instrucciones:
             f.write(texto+'\n')
         return self.leerArchivo(usua,arch)
 
-    def renombrar(self,nom1,nom2,usua):          # rename
+    def renombrar(self,nom1,nom2,usua):             # rename
         os.rename("./"+usua+"/"+nom1,"./"+usua+"/"+nom2)
         return self.verContenido(usua)
 
@@ -77,7 +83,12 @@ class Instrucciones:
         hora=os.path.getmtime("./"+usua+"/"+nombre)
         mensaje=mensaje+'\n\tFecha de modificado: '+str(time.ctime(hora))
         mensaje=mensaje+'\n\tTamaño: '+str(tama)+' bytes'
+        mensaje=mensaje+'\n\t'+os.getcwd()+"/"+usua
         return mensaje
+
+    def accesoPath(self,usua):                      # access
+        req="Acceso para el usuario "+usua
+        return req
 
     def verDireccion(self,usua):                    # pwd
         return os.getcwd()+"/"+usua
